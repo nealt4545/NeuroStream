6c15da118ba8f6549ab7119b160a97233375cb8c
nealt4545 <nealt4545@gmail.com>
2025-02-13 14:35:04 -0500

    Update AnimatedLink.tsx

--- src/components/AnimatedLink.tsx
+++ src/components/AnimatedLink.tsx
@@ -10,13 +10,15 @@ interface AnimatedLinkProps {
 
 export default function AnimatedLink({ href, text }: AnimatedLinkProps) {
   return (
-    <Link href={href} className="text-white hover:text-gray-200 transition">
+    <Link href={href}>
       <motion.span
-        whileHover={{ scale: 1.1, color: "#E0E7FF" }} // Adjust color as desired
+        whileHover={{ scale: 1.1, color: "#E0E7FF" }}
         transition={{ duration: 0.2 }}
+        className="text-white hover:text-gray-200 transition"
       >
         {text}
       </motion.span>
     </Link>
   );
 }
+
