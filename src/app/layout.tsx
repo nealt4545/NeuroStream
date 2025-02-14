cade0034c063f4646dcaaa1e1d38aaec098e9c11
nealt4545 <nealt4545@gmail.com>
2025-02-14 13:03:39 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,20 @@
-// src/app/layout.tsx
-import "./globals.css";
-import ClientWrapper from "../components/ClientWrapper";
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function Document() {
   return (
-    <html lang="en">
-      <head>
-        <title>Neuro Stream</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <Html lang="en">
+      <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap"
           rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto&display=swap"
         />
-      </head>
-      {/* The body is a flex column so that its children can stretch */}
-      <body className="min-h-screen flex flex-col bg-gray-900 text-white">
-        <ClientWrapper>{children}</ClientWrapper>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
       </body>
-    </html>
+    </Html>
   );
 }
+
 
